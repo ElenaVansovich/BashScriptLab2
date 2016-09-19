e0b8cfe21b5c70bbb8c1d10c838bf9e2aedb020b
ElenaVansovich <elena.vansovich.96@mail.ru>
2016-09-19 18:08:44 +0300

    no message

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+TIMEOUT=$(grep "timeout=" ./config.ini | grep -o "[0-9]*")
+
+exec 2> /dev/null
+
 clear
 
 echo "Weather in Minsk:"
@@ -7,10 +11,9 @@ echo "Weather in Minsk:"
 while true
 do
 	list=$(curl http://pogoda.tut.by/city/minsk -silent |
-		 grep '<span class="temp-i">' -m 1 |  
-		sed -e 's/^[ \t]*//g' | 
+		grep '<span class="temp-i">' -m 1 |  
 		sed -e 's/<span class="temp-i">//g' | 
 		sed -e 's/&deg;<\/span><\/div>//g')
 	echo "$list"
-	sleep 2
+	sleep $TIMEOUT
 done
